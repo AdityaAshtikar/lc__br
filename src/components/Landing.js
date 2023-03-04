62450ebc05887347970dcfafa6f04ce87ae1c9ce
aditya.ashtikar <aditya.ashtikar@secninjaz.com>
2023-03-04 22:14:30 +0530

    removed curr lc state

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,21 +1,28 @@
 import React, { useEffect, useRef, useState } from 'react'
-import _1 from '../icons/_1.jpg'
-import _2 from '../icons/_2.jpg'
-import _3 from '../icons/_3.jpg'
-import _4 from '../icons/_4.jpg'
-import _5 from '../icons/_5.jpg'
+// import _1 from '../icons/_1.jpg'
+// import _2 from '../icons/_2.jpg'
+// import _3 from '../icons/_3.jpg'
+// import _4 from '../icons/_4.jpg'
+// import _5 from '../icons/_5.jpg'
 
-const icons = [{
-    id: 1, img: _1,
-    id: 2, img: _2,
-    id: 3, img: _3,
-    id: 4, img: _4,
-    id: 5, img: _5
-}]
+// const icons = [{
+//     id: 1, img: _1,
+//     id: 2, img: _2,
+//     id: 3, img: _3,
+//     id: 4, img: _4,
+//     id: 5, img: _5
+// }]
 
 const Landing = () => {
-    const [currLC, setCurrLC] = useState("")
     const [loc, setLoc] = useState();
+
+    // const [currLC, setCurrLC] = useState()
+    // const currLCRef = useRef(currLC)
+    // const setCurrLCRef = data => {
+    //     currLCRef.current = data;
+    //     setCurrLC(data);
+    // }
+
     const bc = useRef(new BroadcastChannel("lc__br"));
 
     useEffect(() => {
@@ -24,33 +31,23 @@ const Landing = () => {
         navigator.geolocation.getCurrentPosition(lc__successCallback, lc__errorCallback);
     }, []);
 
-    useEffect(() => {
-        console.log("updated: ", currLC)
-    }, [currLC])
-
-    // bc.current.onmessage = (event) => {
-    //     const msg = event.data;
-    //     setCurrLC(msg)
-    // };
-
-    bc.current.addEventListener('message', (event) => {
+    bc.current.onmessage = (event) => {
         const msg = event.data;
-        console.log("msg received: ", msg)
-        setCurrLC(msg)
-    })
+        document.getElementById("currLC").innerHTML = msg
+        // setCurrLCRef(msg);
+    };
 
     function sendNotif() {
         Notification.requestPermission().then(perm => {
             if (perm === "granted" && !!loc.lc__long && !!loc.lc__lat) {
-                const rnd = Math.ceil(Math.random() * 5)
+                // const rnd = Math.ceil(Math.random() * 5)
                 const p = new Notification("Aditya Ashtikar", {
                     body: `Latitude: ${loc.lc__lat}\nLongitute: ${loc.lc__long}`,
-                    icon: `${icons.find(p => p.id === rnd)?.img}`,
+                    // icon: `${icons.find(p => p.id === rnd)?.img}`,
                     tag: "broadcaster"
                 });
 
                 p.addEventListener('show', () => {
-                    console.log("showing notif")
                     bc.current.postMessage(`Current location\nLatitude: ${loc.lc__lat}\nLongitute: ${loc.lc__long}`);
                 })
             }
@@ -62,9 +59,9 @@ const Landing = () => {
             <button onClick={sendNotif} style={{ height: "30vh", width: "60vw", fontSize: "50px", fontWeight: "600" }}> Broadcast</button>
 
             <div style={{ marginTop: "37px", backgroundColor: "cream" }} id="currLC">
-                {currLC}
+                {/* <p>{currLCRef.current || currLC}</p> */}
             </div>
-        </div >
+        </div>
     )
 }
 
